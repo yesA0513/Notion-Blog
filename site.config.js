b9b627c8448a3f9a4eac6c66740cefe919fc1ec4
Noa Kim <kimnoa0513@gmail.com>
2024-06-10 03:51:58 +0900

    Update : edit test

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "김노아",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "",
     bio: "",
     email: "hi@noa.kim",
